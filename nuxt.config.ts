6d8f22662f58cd755b572c165424dbc34e706bda
Skrepy <703189721@qq.com>
2026-03-19 13:17:10 +0800

    修复bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
     ssr: true,
-    app: {
-        baseURL: '/HomePage/'
-    }
+    // app: {
+    //     baseURL: '/HomePage/'
+    // }
 })
